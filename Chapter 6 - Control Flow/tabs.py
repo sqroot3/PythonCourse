74290cd51328bac7b6113c106aa072a65ac801f7
Matias L <argfoo.code@gmail.com>
2018-03-16 16:44:03 -0400

    Started Chapter 6 (Control Flow), added tabs.py

---
+++ Chapter 6 - Control Flow/tabs.py
@@ -0,0 +1,5 @@
+# No braces in python, indentation controls blocks
+
+for i in range(1, 12):
+    print("No {:<2} squared is {:<3} and cubed is {:<4}".format(i, i ** 2, i ** 3))
+    print("Calculation complete")
